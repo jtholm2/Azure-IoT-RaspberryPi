e65c1fd8717e6dcdc99b1ff5530b9669a257ebef
jtholmes <jtholmes@microsoft.com>
2021-07-18 16:56:33 +0000

    Updated rpi-function-app.js

--- Automated Telemetry Ingestion/rpi-function-app.js
+++ Automated Telemetry Ingestion/rpi-function-app.js
@@ -1,16 +1,28 @@
 module.exports = function (context, IoTHubMessage) {
-    //context.log(IoTHubMessage[0])
-    var formattedMessage = JSON.parse(JSON.stringify(IoTHubMessage[0]))
+    
+    context.log(context.bindingData.systemPropertiesArray[0]["iothub-connection-device-id"]);
+    var formattedMessage = JSON.parse(JSON.stringify(IoTHubMessage[0]));
+    context.log(formattedMessage[0])
+
     var mongoClient = require('mongodb').MongoClient;
-    const connString = "{Insert MongoDb connection string here}";
-    const dbName = '{Insert the database name here}';
-    const collectionName = '{Insert the collection name here}';
-    const client = new mongoClient(connString, {useNewUrlParser: true, authSource: dbName})
+    const connString = process.env['COSMOS_MONGO_CONN_STRING'];
+    const dbName = 'rpisurveylogs';
+    const collectionName = context.bindingData.systemPropertiesArray[0]["iothub-connection-device-id"];
+    // const collectionName = 'rpisurvey1';
+    const client = new mongoClient(connString, {useNewUrlParser: true, authSource: dbName});
     async function init() {
+        context.log(client.isConnected()); // false
         await client.connect();
-        context.log(client.isConnected()); //should be true
+        context.log(client.isConnected()); // true
         var collection = client.db(dbName).collection(collectionName);
-        collection.insertMany(formattedMessage);
+        
+        try{
+            await collection.insertMany(formattedMessage);
+            context.log('inserted');
+        } catch (err){
+            context.log(err)
+        }
+
         client.close();
         context.done();
     }
