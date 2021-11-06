29c9b7c2b74c11f733eb4b64f5b28a6234d17cd1
jtholm2 <jtholmes94@gmail.com>
2021-11-06 10:23:38 -0700

    Deleted redundant files, reorganized file structure, and added JS code for the rpi-message-routing function app.

---
+++ iot-edge-module/rpi-survey-data-routing/modules/rpi_survey_routing/main.py
@@ -0,0 +1,102 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+
+import time, os, sys, asyncio, re
+import pandas as pd
+from six.moves import input
+import threading
+from azure.iot.device.aio import IoTHubModuleClient
+from azure.iot.device import Message
+
+async def main():
+    try:
+        if not sys.version >= "3.5.3":
+            raise Exception( "The sample requires python 3.5.3+. Current version of Python: %s" % sys.version )
+        print ( "IoT Hub Client for Python" )
+
+        # The client object is used to interact with your Azure IoT hub.
+        module_client = IoTHubModuleClient.create_from_edge_environment()
+
+        # connect the client.
+        await module_client.connect()
+
+        def formatData(fileToFormat):
+            list = []
+            with open(fileToFormat, mode='r', encoding='utf8') as csv_file:
+                keys = csv_file.readline().strip().split(',')
+                for line in csv_file:
+                    line = line.strip()
+                    row = re.split(r'(?!\s),(?!\s)',line)
+                    list.append(dict(zip(keys, row)))
+
+            formattedCsv = pd.DataFrame(list)
+            droppedDuplicates = formattedCsv.drop_duplicates(subset=[' BSSID', ' Latitude', ' Longitude'])
+            return droppedDuplicates.to_json(orient='records')
+
+        
+        # define behavior for receiving an input message on input1
+        async def scan_start_ingest(module_client):
+            while True:
+                print('starting scan')
+                os.system('screen -d -m sudo airodump-ng --gpsd -w testEndpointOutput --output-format logcsv wlan1mon')
+                time.sleep(15)
+                print('stopping scan')
+                os.system('screen -d -m sudo pkill airodump-ng')
+                print('first screen worked')
+                
+                try:
+                    jsonToSend = formatData('testEndpointOutput-01.log.csv')
+                except (Exception) as e:
+                    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                    message = template.format(type(e).__name__, e.args)
+                    print (message)
+                
+                print('data is formatted')
+                os.system('screen -d -m sudo rm testEndpointOutput-01.log.csv testEndpointOutput-01.gps')
+                print('second screen worked')
+                message = Message(jsonToSend)
+                print('message created')
+                # Send the message.
+                print( "Sending message" )
+                await module_client.send_message_to_output(message, "output1")
+                print ( "Message successfully sent" )
+                time.sleep(1)
+
+        # define behavior for halting the application
+        def stdin_listener():
+            while True:
+                try:
+                    selection = input("Press Q to quit\n")
+                    if selection == "Q" or selection == "q":
+                        print("Quitting...")
+                        break
+                except:
+                    time.sleep(10)
+
+        # Schedule task for C2D Listener
+        listeners = asyncio.gather(scan_start_ingest(module_client))
+
+        print ( "The sample is now waiting for messages. ")
+
+        # Run the stdin listener in the event loop
+        loop = asyncio.get_event_loop()
+        user_finished = loop.run_in_executor(None, stdin_listener)
+
+        # Wait for user to indicate they are done listening for messages
+        await user_finished
+
+        # Cancel listening
+        listeners.cancel()
+
+        # Finally, disconnect
+        await module_client.disconnect()
+
+    except Exception as e:
+        print ( "Unexpected error %s " % e )
+        raise
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
